2e5b76d75d66df9f84f0a900545b4f01dfd3e524
RidhamDiyora <ridhamdiyora7@gmail.com>
2024-10-03 13:51:32 -0400

    commit

---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,16 @@
+import {useContext} from 'react'
+import { StoreContext } from '../../context/StoreContext'
+import './FoodDisplay.css'
+
+const FoodDisplay = ({category}) => {
+
+    const {food_list} = useContext(StoreContext)
+
+  return (
+    <div className='food-display' id='food-display'>
+        <h2>Top Dishes near you</h2>
+    </div>
+  )
+}
+
+export default FoodDisplay
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import "./Home.css"
 import Header from '../../components/Header/Header'
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu"
+import { useState } from "react"
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay"
 
 const Home = () => {
+  const[category, setCategory] = useState('All')
   return (
     <div className="header">
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
