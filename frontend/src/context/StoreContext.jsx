e56842f825b516a42cb7cf6cc5d11b79118daf14
RidhamDiyora <ridhamdiyora7@gmail.com>
2024-10-06 15:20:44 -0400

    commit

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,35 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) =>{
 
+    const [cartItems,SetCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            SetCartItems((prev)=>({...prev,[itemId]:1}))
+        }
+        else{
+            SetCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
+
+    const removeFromCart = (itemId) =>{
+        SetCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        SetCartItems,
+        addToCart,
+        removeFromCart
     }
     
     return(
