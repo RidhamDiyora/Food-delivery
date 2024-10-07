c45da1ef426816746bc20fcec893191018a830e9
RidhamDiyora <ridhamdiyora7@gmail.com>
2024-10-06 22:29:51 -0400

    commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import { assets } from '../../assets/assets'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolorum ipsum, totam ad a odio voluptas quisquam et enim. Commodi at repellendus doloremque! Qui consequuntur deserunt corporis commodi, tempore assumenda?</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+            </div>
+            <div className="footer-content-right">
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
