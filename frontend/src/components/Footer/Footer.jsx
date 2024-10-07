726776851f1e21047b9a7f852162dc73d7781990
RidhamDiyora <ridhamdiyora7@gmail.com>
2024-10-06 22:40:51 -0400

    commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -15,9 +15,23 @@ const Footer = () => {
                 </div>
             </div>
             <div className="footer-content-center">
+              <h2>COMPANY</h2>
+              <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+              </ul>
             </div>
             <div className="footer-content-right">
+              <h2>Get in touch</h2>
+              <ul>
+                <li>+1-222-333-4444</li>
+                <li>contact@tomoto.com</li>
+              </ul>
             </div>
+            <hr />
+            <p className="footer-copyright">Copyright 2024 @ Tomato.com - All rights reserved.</p>
         </div>
     </div>
   )
