32bef49a3f44269ca0974cfd9d5945d97fe8444d
RidhamDiyora <ridhamdiyora7@gmail.com>
2024-12-10 12:57:45 -0500

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,17 @@ import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <>
       <div className="app">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<PlaceOrder />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/order" element={<PlaceOrder />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
-      <Footer />
-    </>
   );
 };
 
