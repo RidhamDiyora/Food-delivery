8de0d270050aa14cd5bbfc2db11b7cee83ce33ca
RidhamDiyora <ridhamdiyora7@gmail.com>
2024-12-18 15:54:41 -0500

    commit

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -6,8 +6,8 @@ const AppDownload = () => {
     <div className="app-download" id="app-download">
         <p>For better exeperience download <br /> our app</p>
         <div className="app-download-platforms">
-            <img src={assets.play_store} alt="" />
-            <img src={assets.app_store} alt="" />
+            <img src={assets.play_store} />
+            <img src={assets.app_store} />
         </div>
     </div>
   )
